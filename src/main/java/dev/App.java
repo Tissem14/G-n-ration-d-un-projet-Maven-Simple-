94ced20223a691fdd597fd97d6ec2624c046f3b2
Tissem14 <tissem.idiripro@gmail.com>
2024-10-16 17:11:53 +0200

    Intégration de la librairie tierce - Jfiglet

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,21 @@
 package dev;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.IOException;
+
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        try {
+            // Conversion de la chaîne "App" en ASCII art
+            String asciiArt = FigletFont.convertOneLine("App");
+
+            // Affichage de l'ASCII art
+            System.out.println(asciiArt);
+        } catch (IOException e) {
+            // Gestion de l'exception IOException
+            System.err.println("Une erreur s'est produite lors de la conversion en ASCII art : " + e.getMessage());
+        }
     }
 }
